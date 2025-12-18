76b0af6959eae0a3a9d317dec2234194ed95c770
Funf <sasha1zaiko@gmail.com>
2025-12-19 00:45:45 +0400

    Написание основной базы данных для категорий, продукта, юзеров

--- basedata.py
+++ basedata.py
@@ -22,31 +22,119 @@ class init_database:
             
 
 class User(db.Model):
-    pass
-class Product(db.Model):
-    pass
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    phone = db.Column(db.String(12), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    FirstName = db.Column(db.String(20))
+    SecondName = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    
+    def __init__(self, username, email, phone, password):
+        self.username = username
+        self.email = email
+        self.phone = phone
+        self.password = password
+        
+    def __repr__(self):
+        return f'<User {self.username}>'
+
 class Category(db.Model):
-    pass
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
 class Second_Category(db.Model):
-    pass
+    __tablename__ = 'second_categories'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    
+    id_categories = db.Column(db.Integer, db.ForeignKey(Category.id))
+    category = db.relationship('Category', backref='second_categories', lazy='dynamic')
+    
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        
+    def __repr__(self):
+        return f'<Second_Category {self.name}>'
+    
 class Third_Category(db.Model):
-    pass
+    __tablename__ = 'third_categories'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    
+    id_second_categories = db.Column(db.Integer, db.ForeignKey(Second_Category.id))
+    category = db.relationship('Second_Category', backref='third_categories', lazy='dynamic')
+    
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        
+    def __repr__(self):
+        return f'<Third_Category {self.name}>'
+    
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    
+    id_third_categories = db.Column(db.Integer, db.ForeignKey(Third_Category.id))
+    third_categories = db.relationship('Third_Category', backref='products', lazy='dynamic')
+        
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    characteristics = db.Column(db.Text)
+    photo = db.Column(db.Text)
+    quantity = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    
+    def __init__(self, name, description=None, characteristics=None, photo=None, quantity=None):
+        self.name = name
+        self.description = description
+        self.characteristics = characteristics
+        self.photo = photo
+        self.quantity = quantity
+        
+    def __repr__(self):
+        return f'<Product {self.name}>'
+    
 class Favorite:
+    __tablename__ = 'favorites'
     pass
 class Favorite_Product(db.Model):
+    __tablename__ = 'favorites_products'
     pass
 class Cars(db.Model):
+    __tablename__ = 'cars'
     pass
 class Cars_Product(db.Model):
+    __tablename__ = 'cars_products'
     pass
 class Comment(db.Model):
+    __tablename__ = 'comments'
     pass
 
 if __name__ == "__main__":
     from flask import Flask
     app = Flask(__name__)
     
-    database = DataBase(app)
+    database = init_database(app)
     
     # Теперь можно работать с БД в контексте приложения
     with app.app_context():
