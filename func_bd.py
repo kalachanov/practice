540791b55b3329b6c2190be82c4a57ff392be2de
Funf <sasha1zaiko@gmail.com>
2026-01-09 20:54:01 +0400

    функции для работы таблицы User

--- func_bd.py
+++ func_bd.py
@@ -38,6 +38,7 @@ class user:
                     return False
             except Exception as e:
                 return f'Произошла ошибка:\n{e}' 
+            
         @staticmethod
         def get_by_username(username: str):
             try:
@@ -45,7 +46,60 @@ class user:
                     .filter(User.username == username).first()
                 return user
             except Exception as e:
+                return f'Произошла ошибка:\n{e}'
+            
+        @staticmethod
+        def get_by_id(id: int):
+            try:
+                user = User.query\
+                    .filter(User.id == id).first()
+                return user
+            except Exception as e:
+                return f'Произошла ошибка:\n{e}'
+            
+        @staticmethod
+        def delete_by_id(id: int):
+            try:
+                user = User.query\
+                    .filter(User.id == id).delete()
+                db.session.commit()
+                return True
+            except Exception as e:
                 return f'Произошла ошибка:\n{e}' 
+            
+        @staticmethod
+        def change_by_id(id: int, username: str = None, password: str = None, first_name: str = None, 
+                 second_name: str = None, email: str = None, phone: str = None):
+            try:
+                user = User.query\
+                    .filter(User.id== id).first()
+                if username: user.username = username
+                if password: user.password = password
+                if email: user.email = email
+                if phone: user.phone = phone
+                if first_name: user.first_name = first_name
+                if second_name: user.second_name = second_name
+                db.session.commit()
+                return user
+            except Exception as e:
+                return f'Произошла ошибка:\n{e}'  
+            
+        @staticmethod
+        def change_by_username(username_first: str, username: str = None, password: str = None, first_name: str = None, 
+                 second_name: str = None, email: str = None, phone: str = None):
+            try:
+                user = User.query\
+                    .filter(User.username== username_first).first()
+                if username: user.username = username
+                if password: user.password = password
+                if email: user.email = email
+                if phone: user.phone = phone
+                if first_name: user.first_name = first_name
+                if second_name: user.second_name = second_name
+                db.session.commit()
+                return user
+            except Exception as e:
+                return f'Произошла ошибка:\n{e}'  
                    
     class bd_cart_product:
         pass
@@ -67,4 +121,4 @@ if __name__ == "__main__":
     
     with app.app_context():
         pass
-        print(user.bd_user.get_by_username('Funf'))
+        print(admin.bd_user.change_by_id(id=4, email='XXX@gmail'))
