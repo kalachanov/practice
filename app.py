402becbc109a83d68b278dd1d8bf6a7d1978e353
Funf <sasha1zaiko@gmail.com>
2026-01-13 05:34:54 +0400

    НАЧАЛО разработки приложения

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/main.html')
+def main():
+    return render_template('main.html')
+
+@app.route('/catalog.html')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/favourites.html')
+def favorite():
+    return render_template('favorite.html')
+
+if __name__ == '__main__':
+    app.run()
