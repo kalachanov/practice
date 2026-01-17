5f18f30a4e451af6caed7de0ddec8e401f0ff932
Funf <sasha1zaiko@gmail.com>
2026-01-17 16:09:13 +0300

    Отделение наработок от основы

--- app.py
+++ app.py
@@ -8,13 +8,25 @@ def main():
     products = admin.bd_product.get_all_by_third_category_id(1)
     return render_template('main_flask.html', products = products)
 
-@app.route('/catalog.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/profil')
+def profile():
+    return render_template('profil.html')
+
+@app.route('/catalog')
 def catalog():
     return render_template('catalog.html')
 
-@app.route('/favourites.html')
+@app.route('/favourites')
 def favorite():
     return render_template('favorite.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
